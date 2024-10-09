159758ab73be381df3a2548bf6037f175b303a76
hamdi <hamdi.bennosra.dev@gmail.com>
2024-10-09 16:07:09 +0100

    layout fixes

--- src/app/layout/service/app.layout.service.ts
+++ src/app/layout/service/app.layout.service.ts
@@ -35,7 +35,7 @@ export class LayoutService {
     config = signal<AppConfig>(this._config);
 
     state: LayoutState = {
-        staticMenuDesktopInactive: false,
+        staticMenuDesktopInactive: true,
         overlayMenuActive: false,
         profileSidebarVisible: false,
         configSidebarVisible: false,
@@ -98,7 +98,7 @@ export class LayoutService {
     }
 
     showConfigSidebar() {
-        this.state.configSidebarVisible = true;
+        this.state.configSidebarVisible = false;
     }
 
     isOverlay() {
